b13da61c855c11305d49f48d1e89c0e11482e960
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2023-09-19 13:13:04 -0700

    last fix

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use('/api/bookings', bookingsRoute);
 __dirname = path.resolve();
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 } else {
     app.get("/", function (req, res) {
