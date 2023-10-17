25782677078e1c0eef8db5cf2ccb4e1857c28ee6
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2023-10-16 23:52:11 -0700

    fixedpicsspclevents

--- client/src/screens/Specialeventsscreen.js
+++ client/src/screens/Specialeventsscreen.js
@@ -20,55 +20,71 @@ const Specialeventsscreen = () => {
       <h4 style={{textAlign: 'center', color: 'darkorange'}}>It is a high honor to be able to host your celebration with your loved ones.</h4>
       <TextSection paragraph="Whether you are having a birthday party or a big celebration, let us host your event here! Little Heaven Bed & Breakfast is secluded so you can enjoy your party to the biggest. We have a commercial kitchen with seating, outdoor space, a pond, and of course six guest rooms with six guest bathrooms. Call the cabin for more information and pricing."/>
         
-        <section stle={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0', paddingBottom: '0', marginLeft: '1rem', marginRight: '1rem',width: '100%', height: '100%' }}>
-    <div style={{ display: 'block', width: 'auto', padding: '7%', justifyContent: 'center' }}>
+        <section stle={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0', paddingBottom: '0', marginLeft: '1rem', marginRight: '1rem',width: '50%', height: 'auto' }}>
+    
+      <div style={{ display: 'block', flexDirection: 'row' }}>
             
-            <h1 style={{ textAlign: 'center', marginBottom: '4rem', color: 'rgba(240, 116, 8, 0.848)' }}>Check out our upcoming events!</h1>
+            <h1 style={{ textAlign: 'center', verticalAlign:'middle', marginBottom: '4rem', color: 'rgba(240, 116, 8, 0.848)' }}>Check out our upcoming events!</h1>
          
-            <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center'}}>
-                <article className="room" data-aos='zoom-in-down'
+            <Row>
+           <Col>
+           <article className="room" data-aos='zoom-in-down'
                   data-aos-duration='1200'
                   data-aos-anchor-placement='center top'>
                   <h2 className="room-info">October</h2>
                   <br />
+                  
                   <div className="img-container">
-                    <img src={Tour} alt="single card" className="smallimg" />
+                    <img src={Tour} alt="single card" className="smallimgtwo" />
                     <Link to={'https://www.facebook.com/events/698739138355698?acontext=%7B%22event_action_history%22%3A[]%7D'} className="btn-primary room-link">
                       <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page.</h2>
                     </Link>
                   </div>
                 </article>
-                  <br/>
-                  <br/>
-                <article className="room" data-aos='zoom-in-down'
+           </Col>
+                
+            
+         
+                  <Col>
+                  <article className="room" data-aos='zoom-in-down'
                   data-aos-duration='1200'
                   data-aos-anchor-placement='center bottom'>
                   <h2 className="room-info">November</h2>
                   <br />
                   <div className="img-container">
-                    <img src={Treats} alt="single card" className="smallimg" />
+                    <img src={Treats} alt="single card" className="smallimgtwo" />
                     <Link to={'https://www.facebook.com/events/s/thanksgiving-dinner-celebratio/1053085902489168/?mibextid=RQdjqZ&paipv=0&eav=AfYwHHZR7XVCiBUQulFbkiOhDtl5qvAtbNmmTUTH2Nx8qIrmKoaPVoYjt2JuDPc-g34&_rdr'} className="btn-primary room-link">
                       <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page</h2>
                     </Link>
                   </div>
                 </article>
-             <br/>
-             <br/>
-                <article className="room" data-aos='zoom-in-down'
+                  </Col>
+               
+          <Col>
+          <article className="room" data-aos='zoom-in-down'
                   data-aos-duration='1200'
                   data-aos-anchor-placement='center bottom'>
                   <h2 className="room-info">December</h2>
                   <br />
-                  <div className="img-container">
-                    <img src={Directions} alt="single card" className="smallimg" />
+                  <div className="img-container" style={{objectFit: "scale-down"}}>
+                    <img src={Directions} alt="single card" className="smallimgtwo" />
                     <Link to={'https://www.facebook.com/events/281155201478354/?paipv=0&eav=AfaX1-YT36A8bMddkUJ3WaoDb6WwfsGCOhzo8WNEX_HK5sakwWhN77-xGztD33ObaK0&_rdr'} className="btn-primary room-link">
                       <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page</h2>
                     </Link>
                   </div>
                 </article>
+          </Col>
+              
   
             </Row>
             </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
 
             <h1 style={{ textAlign: 'center', marginBottom: '4rem', color: 'rgba(240, 116, 8, 0.848)' }}>Don't miss out on the fun! We look forward to seeing you at the Cabin. Thank you!</h1>
 </section>
