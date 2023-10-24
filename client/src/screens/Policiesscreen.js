292fe9a8330626437dace05b26822eb3bd8f32ac
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2023-10-24 15:54:26 -0700

    specialeventsupdate

--- client/src/screens/Policiesscreen.js
+++ client/src/screens/Policiesscreen.js
@@ -1,9 +1,6 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import { SliderDataTen } from '../data/SliderData'
-import Cards from '../components/Cards'
-import TextSection from '../components/TextSection'
-import GalleryThree from '../components/GalleryThree'
+import React from 'react';
+import Hero from '../components/Hero';
+import { SliderDataTen } from '../data/SliderData';
 
 const Policiesscreen = () => {
   return (
@@ -14,7 +11,7 @@ const Policiesscreen = () => {
     <br/>
     <br/>
     <h1>Policies</h1>
-      <h4 style={{textAlign: 'center', color: 'darkorange'}}>at Little Heaven Bed & Breakfast</h4>
+      <h4 style={{textAlign: 'center', color: 'darkorange', marginLeft: '3rem', marginRight: '3rem'}}>at Little Heaven Bed & Breakfast</h4>
       <br/>
       <b><p style={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0',paddingBottom: '0', marginLeft: '3rem', marginRight: '3rem'}}>Cancellations:</p></b><p data-aos="fade-right"
           data-aos-duration="1300"
@@ -23,6 +20,8 @@ const Policiesscreen = () => {
           We understand that things can happen that prevent us from doing the things we planned. We ask that if you wish to cancel your reservations you let us know 48 hours before your reservation. We will refund your money either by check or through direct deposit.
       </p>
       <br/>   
+
+
       <b><p style={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0',paddingBottom: '0', marginLeft: '3rem', marginRight: '3rem'}}>Prices:</p></b><p data-aos="fade-right"
           data-aos-duration="1300"
           data-aos-anchor-placement="center bottom"
@@ -30,14 +29,18 @@ const Policiesscreen = () => {
           are based on the occupancy of the room, the features, and the dates that the room is being booked for. Prices do go up for Friday and Saturday.
       </p>
       <br/>
+
+
       <b><p style={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0',paddingBottom: '0', marginLeft: '3rem', marginRight: '3rem'}}>Room Accommodations:</p></b><p data-aos="fade-right"
           data-aos-duration="1300"
           data-aos-anchor-placement="center bottom"
           style={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0',paddingBottom: '0', marginLeft: '3rem', marginRight: '3rem'}}>
         
-          Check in is at 3:00 p.m. and check out is at 11:00 a.m. every day. Our rooms feature a queen or king-size bed, one or two sitting chairs, a desk, a high definition TV, a tub or standing shower, toilet, an iron, ironing board, a fireplace, and windows. If you wish to request any additional items please let the cabin know with time. 
+          Check in is at 3:00 p.m. and check out is at 11:00 a.m. every day. In your room you will find the following items: A welcome package, a gift of goodies, an iron and ironing board, four luxurious towels, high quality blankets, hand soap, shampoo, conditioner, body wash, body lotion, make up remover, disposable shower caps, personal wipes, toothbrush and toothpaste, an elegant bathrobe, water bottles, and wine cups. We want you to have the best experience possible, please contact the cabin in advance if you would like any special requests. Thank you once again for choosing to stay at Little Heaven Bed and Breakfast. 
       </p>
       <br/>
+
+
       <b><p style={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0',paddingBottom: '0', marginLeft: '3rem', marginRight: '3rem'}}>Children:</p></b><p data-aos="fade-right"
           data-aos-duration="1300"
           data-aos-anchor-placement="center bottom"
@@ -45,6 +48,8 @@ const Policiesscreen = () => {
           We ask that only people 18 years of age or older book a room, and all guests be at least 12 years of age or older. We want to maintain the cabin a quiet, adult friendly space for everyone's convenience and comfort. 
       </p>
       <br/>
+
+
       {/* <b><p style={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0',paddingBottom: '0', marginLeft: '3rem', marginRight: '3rem'}}>Minimum Stay:</p></b><p data-aos="fade-right"
           data-aos-duration="1300"
           data-aos-anchor-placement="center bottom"
@@ -52,6 +57,8 @@ const Policiesscreen = () => {
           We please ask that you have a minimum of two nights stay so we can provide the best service possible. 
       </p> */}
       <br/>
+
+
       <b><p style={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0',paddingBottom: '0', marginLeft: '3rem', marginRight: '3rem'}}>Breakfast:</p></b><p data-aos="fade-right"
           data-aos-duration="1300"
           data-aos-anchor-placement="center bottom"
--- client/src/screens/Specialeventsscreen.js
+++ client/src/screens/Specialeventsscreen.js
@@ -1,95 +1,127 @@
 import React from 'react';
 import TextSection from '../components/TextSection';
-import InfoSection6 from '../components/InfoSection6';
-import { InfoDataSeven } from '../data/InfoData';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Tour from '../images/pumpkin.jpg';
 import Directions from '../images/christmas.jpg';
-import Policies from '../images/policy.jpg';
 import Treats from '../images/thanksgiving.JPEG';
 import { Link } from 'react-router-dom';
+import Photos from '../components/Photos';
+import '../components/Photos.css';
+
 
 const Specialeventsscreen = () => {
+
+  const galleryImages = [
+    {
+      img: 'https://i.postimg.cc/503wByC0/image0.jpg'
+    },
+    {
+      img: 'https://i.postimg.cc/mgHCd8HK/image1.jpg'
+    },
+    {
+      img: 'https://i.postimg.cc/wv0mtrCR/image2.jpg'
+    },
+    {
+      img: 'https://i.postimg.cc/yYJZdvvW/image3.jpg'
+    },
+    {
+      img: 'https://i.postimg.cc/5yDjLWny/image4.jpg'
+    },
+    {
+      img: 'https://i.postimg.cc/hGMjMdpH/image5.jpg'
+    },
+    {
+      img: 'https://i.postimg.cc/pX8KCs84/image6.jpg'
+    },
+    {
+      img: 'https://i.postimg.cc/TPPyz0LR/image8.jpg'
+    },
+    {
+      img: 'https://i.postimg.cc/Zq0R7q2n/image9.jpg'
+    }
+  ]
+
   return (
     <>
-    <br/>
-    <br/>
-    <br/>
-    <h1>Special Events & Celebrations</h1>
-      <h4 style={{textAlign: 'center', color: 'darkorange'}}>It is a high honor to be able to host your celebration with your loved ones.</h4>
-      <TextSection paragraph="Whether you are having a birthday party or a big celebration, let us host your event here! Little Heaven Bed & Breakfast is secluded so you can enjoy your party to the biggest. We have a commercial kitchen with seating, outdoor space, a pond, and of course six guest rooms with six guest bathrooms. Call the cabin for more information and pricing."/>
-        
-        <section stle={{fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0', paddingBottom: '0', marginLeft: '1rem', marginRight: '1rem',width: '50%', height: 'auto' }}>
-    
-      <div style={{ display: 'block', flexDirection: 'row' }}>
-            
-            <h1 style={{ textAlign: 'center', verticalAlign:'middle', marginBottom: '4rem', color: 'rgba(240, 116, 8, 0.848)' }}>Check out our upcoming events!</h1>
-         
-            <Row>
-           <Col>
-           <article className="room" data-aos='zoom-in-down'
-                  data-aos-duration='1200'
-                  data-aos-anchor-placement='center top'>
-                  <h2 className="room-info">October</h2>
-                  <br />
-                  
-                  <div className="img-container">
-                    <img src={Tour} alt="single card" className="smallimgtwo" />
-                    <Link to={'https://www.facebook.com/events/698739138355698?acontext=%7B%22event_action_history%22%3A[]%7D'} className="btn-primary room-link">
-                      <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page.</h2>
-                    </Link>
-                  </div>
-                </article>
-           </Col>
-                
-            
-         
-                  <Col>
-                  <article className="room" data-aos='zoom-in-down'
-                  data-aos-duration='1200'
-                  data-aos-anchor-placement='center bottom'>
-                  <h2 className="room-info">November</h2>
-                  <br />
-                  <div className="img-container">
-                    <img src={Treats} alt="single card" className="smallimgtwo" />
-                    <Link to={'https://www.facebook.com/events/s/thanksgiving-dinner-celebratio/1053085902489168/?mibextid=RQdjqZ&paipv=0&eav=AfYwHHZR7XVCiBUQulFbkiOhDtl5qvAtbNmmTUTH2Nx8qIrmKoaPVoYjt2JuDPc-g34&_rdr'} className="btn-primary room-link">
-                      <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page</h2>
-                    </Link>
-                  </div>
-                </article>
-                  </Col>
-               
-          <Col>
-          <article className="room" data-aos='zoom-in-down'
-                  data-aos-duration='1200'
-                  data-aos-anchor-placement='center bottom'>
-                  <h2 className="room-info">December</h2>
-                  <br />
-                  <div className="img-container" style={{objectFit: "scale-down"}}>
-                    <img src={Directions} alt="single card" className="smallimgtwo" />
-                    <Link to={'https://www.facebook.com/events/281155201478354/?paipv=0&eav=AfaX1-YT36A8bMddkUJ3WaoDb6WwfsGCOhzo8WNEX_HK5sakwWhN77-xGztD33ObaK0&_rdr'} className="btn-primary room-link">
-                      <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page</h2>
-                    </Link>
-                  </div>
-                </article>
-          </Col>
-              
-  
-            </Row>
-            </div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+      <br />
+      <br />
+      <br />
+      <h1>Special Events & Celebrations</h1>
+      <section stle={{ fontSize: '1.5rem', color: 'black', textAlign: 'center', paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '0', paddingBottom: '0', marginLeft: '1rem', marginRight: '1rem', width: '50%', height: 'auto' }}>
 
-            <h1 style={{ textAlign: 'center', marginBottom: '4rem', color: 'rgba(240, 116, 8, 0.848)' }}>Don't miss out on the fun! We look forward to seeing you at the Cabin. Thank you!</h1>
-</section>
+        <div style={{ display: 'block', flexDirection: 'row' }}>
 
-  </>
+          <h1 style={{ textAlign: 'center', verticalAlign: 'middle', marginBottom: '4rem', color: 'rgba(240, 116, 8, 0.848)' }}>Check out our upcoming events!</h1>
+
+          <Row>
+            <Col>
+              <article className="room" data-aos='zoom-in-down'
+                data-aos-duration='1200'
+                data-aos-anchor-placement='center top'>
+                <h2 className="room-info">October</h2>
+                <br />
+
+                <div className="img-container">
+                  <img src={Tour} alt="single card" className="smallimgtwo" />
+                  <Link to={'https://www.facebook.com/events/698739138355698?acontext=%7B%22event_action_history%22%3A[]%7D'} className="btn-primary room-link">
+                    <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page.</h2>
+                  </Link>
+                </div>
+              </article>
+            </Col>
+
+            <Col>
+              <article className="room" data-aos='zoom-in-down'
+                data-aos-duration='1200'
+                data-aos-anchor-placement='center bottom'>
+                <h2 className="room-info">November</h2>
+                <br />
+                <div className="img-container">
+                  <img src={Treats} alt="single card" className="smallimgtwo" />
+                  <Link to={'https://www.facebook.com/events/s/thanksgiving-dinner-celebratio/1053085902489168/?mibextid=RQdjqZ&paipv=0&eav=AfYwHHZR7XVCiBUQulFbkiOhDtl5qvAtbNmmTUTH2Nx8qIrmKoaPVoYjt2JuDPc-g34&_rdr'} className="btn-primary room-link">
+                    <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page</h2>
+                  </Link>
+                </div>
+              </article>
+            </Col>
+
+            <Col>
+              <article className="room" data-aos='zoom-in-down'
+                data-aos-duration='1200'
+                data-aos-anchor-placement='center bottom'>
+                <h2 className="room-info">December</h2>
+                <br />
+                <div className="img-container" style={{ objectFit: "scale-down" }}>
+                  <img src={Directions} alt="single card" className="smallimgtwo" />
+                  <Link to={'https://www.facebook.com/events/281155201478354/?paipv=0&eav=AfaX1-YT36A8bMddkUJ3WaoDb6WwfsGCOhzo8WNEX_HK5sakwWhN77-xGztD33ObaK0&_rdr'} className="btn-primary room-link">
+                    <h2 style={{ color: 'white', textDecoration: 'underline' }}>Go to Facebook Page</h2>
+                  </Link>
+                </div>
+              </article>
+            </Col>
+
+
+          </Row>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h4 style={{ textAlign: 'center', color: 'darkorange' }}>It is a high honor to be able to host your celebration with your loved ones.</h4>
+        <TextSection paragraph="Whether you are having a birthday party or a big celebration, let us host your event here! Little Heaven Bed & Breakfast is secluded so you can enjoy your party to the biggest. We have a conference room, a commercial kitchen with seating, outdoor space, a pond, and of course six guest rooms with six guest bathrooms. Check out our conference room below! Call the cabin to reserve your special day now." />
+       
+
+      </section>
+
+      <Photos galleryImages={galleryImages} />
+      <br/>
+      <h2 style={{ textAlign: 'center', marginBottom: '4rem', color: 'rgba(240, 116, 8, 0.848)' }}>Don't miss out on the fun! We look forward to seeing you at the Cabin. Thank you!</h2>
+      <br/>
+    </>
   )
 }
 
